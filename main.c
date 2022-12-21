1fac8951e6643594b377e91426da037e96aab34b
Tono Villarejo Reig <tonovillarejo@gmail.com>
2022-12-21 16:04:54 +0100

    prueba 100 main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 16:44:34 by tvillare          #+#    #+#             */
-/*   Updated: 2022/12/21 15:59:34 by tvillare         ###   ########.fr       */
+/*   Updated: 2022/12/21 16:04:17 by tvillare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,31 +58,31 @@ int main(int argc, char **argv)
 	created_index(list_a);
 	list_a = find_first_list(list_a);
 
-	/*list_b = ft_calloc(1, sizeof(t_order));
+	list_b = ft_calloc(1, sizeof(t_order));
 	list_a = move_block_b(list_a, list_b, 12, 15);
-	return_to_a(list_a, list_b);
-	print_cosas(list_a);*/
+	return_to_a(list_a, list_b, 4);
+	print_cosas(list_a);
 
-	//list_b = ft_calloc(1, sizeof(t_order));
-	//list_a = move_block_b(list_a, list_b, 8, 11);
-	hundred_number(list_a);
-	/*min_to_top (list_a, 11);
+	list_b = ft_calloc(1, sizeof(t_order));
+	list_a = move_block_b(list_a, list_b, 8, 11);
+	//hundred_number(list_a);
+	min_to_top (list_a, 11);
 	print_cosas(list_a);
 	ft_printf("---------\n");
-	return_to_a(list_a, list_b);
+	return_to_a(list_a, list_b, 4);
 	print_cosas(list_a);
 
 	list_b = ft_calloc(1, sizeof(t_order));
 	list_a = move_block_b(list_a, list_b, 4, 7);
 	min_to_top (list_a, 7);
-	return_to_a(list_a, list_b);
+	return_to_a(list_a, list_b, 4);
 
 	//min_to_top (list_a, 3);
 	list_b = ft_calloc(1, sizeof(t_order));
 	list_a = move_block_b(list_a, list_b, 0, 3);
 	min_to_top (list_a, 3);
-	return_to_a(list_a, list_b);
-	min_to_top (list_a, -1);*/
+	return_to_a(list_a, list_b, 4);
+	min_to_top (list_a, -1);
 
 	list_a = find_first_list(list_a);
 	print_cosas(list_a);
